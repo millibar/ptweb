05ceb023871384caee832cd28ad841039b77acd1
Motonori Banno <millibarjp@gmail.com>
2021-01-11 15:57:46 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,100 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var helmet = require('helmet');
+
+// Twitter認証用
+var session = require('express-session');
+var passport = require('passport');
+var TwitterStrategy = require('passport-twitter').Strategy;
+
+var TWITTER_CONSUMER_KEY = 'modkhta63SvE68i3nuLnaxohR';
+var TWITTER_CONSUMER_SECRET = 'XITbP4g5Eu0rEmPQutH08MpFZw20cfEQB4k7o0htu44XFfOqwp';
+
+// 認証情報をセッションで管理
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new TwitterStrategy({
+    consumerKey: TWITTER_CONSUMER_KEY,
+    consumerSecret : TWITTER_CONSUMER_SECRET,
+    callbackURL: 'http://localhost:8000/auth/twitter/callback'
+  },
+  function(token, tokenSecret, profile, done) {
+
+    process.nextTick(function() {
+      return done(null, profile);
+    });
+  }
+));
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+app.use(helmet());
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// セッション用（Twitter認証で使う）
+app.use(session({ secret: 'hirothecat2021', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', indexRouter);
+app.use('/users', ensureAuthenticated, usersRouter);
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback',
+  passport.authenticate('twitter', { successRedirect: '/', 
+                                     failureRedirect: '/login'}));
+
+app.get('/login', function(req, res) {
+  res.render('login');
+});
+
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+// Routerを登録するapp.use関数に与える第2引数用の関数。そのパスへのアクセスに認証を必要とさせる。
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next() }
+  res.redirect('/login');
+}
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
