cfe4dcfe51e8cfeef66670e7a761b68b29a9fe76
Motonori Banno <millibarjp@gmail.com>
2021-06-06 12:20:25 +0900

    log追加

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -4,7 +4,6 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/prisoner_training',
   {
-    operatorsAliases: false,
     dialectOptions: {
       ssl: {
         require: true,
