f0c42e8dfbb59cc058f7456631fd1ea8747beec0
millibar <millibarjp@gmail.com>
2021-08-14 12:16:25 +0900

    HEROKU_URLをAPP_URLに変更

--- secret.js
+++ secret.js
@@ -2,7 +2,7 @@
 
 const TWITTER_CONSUMER_KEY = process.env.TWITTER_CONSUMER_KEY || 'modkhta63SvE68i3nuLnaxohR';
 const TWITTER_CONSUMER_SECRET = process.env.TWITTER_CONSUMER_SECRET || 'XITbP4g5Eu0rEmPQutH08MpFZw20cfEQB4k7o0htu44XFfOqwp';
-const CALLBACK_URL = process.env.HEROKU_URL ? process.env.HEROKU_URL + '/auth/twitter/callback' : 'http://localhost:8000/auth/twitter/callback';
+const CALLBACK_URL = process.env.APP_URL ? process.env.APP_URL + '/auth/twitter/callback' : 'http://localhost:8000/auth/twitter/callback';
 const SESSION_SECRET = 'hirothecat2021';
 
 module.exports = {
